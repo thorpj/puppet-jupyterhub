f9c18c744d4a1128f59b8dcc464bf0acb3b25dcf
Rike-Benjamin Schuppner <rikebs@debilski.de>
2015-10-10 15:51:50 +0200

    BF: Module prefix for template files.

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,24 +6,24 @@ class jupyterhub::config {
 
   file { "${::jupyterhub::jupyterhub_dir}/jupyterhub_config.py":
     owner   => $::jupyterhub::jupyterhub_username,
-    content => template('jupyterhub_config.py.erb'),
+    content => template("${module_name}/jupyterhub_config.py.erb"),
   }
 
   file { '/etc/sudoers.d/sudospawner':
     owner   => $::jupyterhub::jupyterhub_username,
-    content => template('jupyterhub_sudoers.erb'),
+    content => template("${module_name}/jupyterhub_sudoers.erb"),
     require => Class[ profile::sudo ],
   }
 
   file { "${::jupyterhub::jupyterhub_dir}/start_jupyterhub.sh":
     owner   => $::jupyterhub::jupyterhub_username,
     mode    => '0755',
-    content => template('start_jupyterhub.sh.erb'),
+    content => template("${module_name}/start_jupyterhub.sh.erb"),
   }
 
   file { "/etc/systemd/system/${::jupyterhub::service_name}.service":
     owner   => 'root',
-    content => template('jupyterhub.service.erb'),
+    content => template("${module_name}/jupyterhub.service.erb"),
   }
   ~>
   Exec[ systemctl_daemon-reload ]
