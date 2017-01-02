509cbeb58b25a5b3175ac750c320829c4a9ded9f
Bruno Marques <bruno@bmarques.net>
2017-01-02 13:32:49 -0200

    Ensure creation of log file

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,11 @@
 #
 class jupyterhub::config {
 
+  file { '/var/log/jupyterhub.log':
+    owner   => $::jupyterhub::jupyterhub_username,
+    ensure  => present,
+  }
+
   file { "${::jupyterhub::jupyterhub_dir}/jupyterhub_config.py":
     owner   => $::jupyterhub::jupyterhub_username,
     content => template("${module_name}/jupyterhub_config.py.erb"),
