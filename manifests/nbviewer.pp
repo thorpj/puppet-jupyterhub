34b08c867fdf1731ef3dde853620b27f834ef16b
Bruno Marques <bomarques@inf.ufrgs.br>
2017-01-16 14:44:05 -0200

    Copying stuff to nbviewer module

--- manifests/nbviewer.pp
+++ manifests/nbviewer.pp
@@ -2,7 +2,8 @@ class jupyterhub::nbviewer {
     ensure_packages(['libmemcached-dev', 'libcurl4-openssl-dev', 'pandoc',
                      'libevent-dev'])
     $python_version = hiera('python::version')
-    $nbviewer_path = "${$::jupyterhub::pyvenv}/src/nbviewer"
+    $nbviewer_src = "${$::jupyterhub::pyvenv}/src/nbviewer"
+    $nbviewer_lib = "${$::jupyterhub::pyvenv}/lib/python$python_version/site-packages/nbviewer"
 
     python::pip { 'nbviewer':
       pkgname    => 'git+https://github.com/jupyter/nbviewer',
@@ -13,24 +14,32 @@ class jupyterhub::nbviewer {
     exec { 'nbviewer-npm-install':
       command     => '/usr/bin/npm install',
       timeout     => 900,  # 15 minutes
-      cwd         => $nbviewer_path,
+      cwd         => $nbviewer_src,
       refreshonly => true,
       require     => [Package['npm'], Package['nodejs-legacy']],
     } ~>
     # we need to provide a dummy stdin so that invoke doesn't choke badly
     exec { 'echo "bower" | invoke -p bower':
       path        => "${$::jupyterhub::pyvenv}/bin:/usr/bin",
-      cwd         => $nbviewer_path,
+      cwd         => $nbviewer_src,
       refreshonly => true,
       provider    => shell,
       require     => Python::Pip['invoke'],
     } ~>
     exec { 'echo "less" | invoke -p less':
       path        => "${$::jupyterhub::pyvenv}/bin:/usr/bin",
-      cwd         => $nbviewer_path,
+      cwd         => $nbviewer_src,
       refreshonly => true,
       provider    => shell,
       require     => Python::Pip['pip-9.0.1'],
+    } ~>
+    file { "$nbviewer_lib/static/build":
+      ensure => directory,
+      source => "$nbviewer_src/nbviewer/static/build",
+    } ~>
+    file { "$nbviewer_lib/static/components":
+      ensure => directory,
+      source => "$nbviewer_src/nbviewer/static/components",
     }
 
     python::pip { 'markdown':
