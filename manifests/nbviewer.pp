ef81f0aacba15e3d5996b78b3abdceb15fa6cf4c
Bruno Marques <bomarques@inf.ufrgs.br>
2017-01-16 10:44:52 -0200

    Installing nbviewer via pip

--- manifests/nbviewer.pp
+++ manifests/nbviewer.pp
@@ -2,15 +2,6 @@ class jupyterhub::nbviewer {
     ensure_packages(['libmemcached-dev', 'libcurl4-openssl-dev', 'pandoc',
                      'libevent-dev'])
 
-    $nbviewer_path = "${::jupyterhub::dir}/nbviewer"
-
-    vcsrepo { $nbviewer_path:
-      ensure   => latest,
-      revision => master,
-      provider => git,
-      source   => 'https://github.com/jupyter/nbviewer.git',
-    }
-
     python::requirements { "$nbviewer_path/requirements.txt":
       virtualenv => "$::jupyterhub::pyvenv",
       owner      => $::jupyterhub::jupyterhub_username,
@@ -22,19 +13,25 @@ class jupyterhub::nbviewer {
       owner      => $::jupyterhub::jupyterhub_username,
       require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
     } ~>
-    exec { 'nbviewer-npm-install':
-      command     => 'npm install -g',
-      timeout     => 900,  # 15 minutes
-      path        => $nbviewer_path,
-      refreshonly => true,
-      subscribe   => Vcsrepo[$nbviewer_path],
-    } ~>
-    exec { 'invoke bower':
-      path        => $nbviewer_path,
-      refreshonly => true,
-    } ~>
-    exec { 'invoke less':
-      path        => $nbviewer_path,
-      refreshonly => true,
-    }
+    python::pip { 'nbviewer':
+      pkgname    => 'git+https://github.com/jupyter/nbviewer',
+      virtualenv => "$::jupyterhub::pyvenv",
+      owner      => $::jupyterhub::jupyterhub_username,
+      require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
+    }# ~>
+    #exec { 'nbviewer-npm-install':
+    #  command     => 'npm install -g',
+    #  timeout     => 900,  # 15 minutes
+    #  path        => $nbviewer_path,
+    #  refreshonly => true,
+    #  subscribe   => Vcsrepo[$nbviewer_path],
+    #} ~>
+    #exec { 'invoke bower':
+    #  path        => $nbviewer_path,
+    #  refreshonly => true,
+    #} ~>
+    #exec { 'invoke less':
+    #  path        => $nbviewer_path,
+    #  refreshonly => true,
+    #}
 }
