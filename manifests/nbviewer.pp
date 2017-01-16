3ee45407d7664203b8466d969efc9dce2d8ac7ed
Bruno Marques <bomarques@inf.ufrgs.br>
2017-01-16 10:56:41 -0200

    Added requirements.txt from nbviewer

--- manifests/nbviewer.pp
+++ manifests/nbviewer.pp
@@ -1,29 +1,27 @@
 class jupyterhub::nbviewer {
     ensure_packages(['libmemcached-dev', 'libcurl4-openssl-dev', 'pandoc',
                      'libevent-dev'])
+    $nbviewer_path = "${$::jupyterhub::pyvenv}/lib/python%{hiera('python::version')}/site-packages/nbviewer"
 
     python::pip { 'nbviewer':
       pkgname    => 'git+https://github.com/jupyter/nbviewer',
       virtualenv => "$::jupyterhub::pyvenv",
       owner      => $::jupyterhub::jupyterhub_username,
       require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
+    } ~>
+    python::requirements { "$nbviewer_path/requirements.txt":
+      virtualenv => "$::jupyterhub::pyvenv",
+      owner      => $::jupyterhub::jupyterhub_username,
     }# ~>
-    #python::requirements { "$nbviewer_path/requirements.txt":
-    #  virtualenv => "$::jupyterhub::pyvenv",
-    #  owner      => $::jupyterhub::jupyterhub_username,
-    #  require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
-    #} ~>
     #python::requirements { "$nbviewer_path/requirements-dev.txt":
     #  virtualenv => "$::jupyterhub::pyvenv",
     #  owner      => $::jupyterhub::jupyterhub_username,
-    #  require    => Python::Pyvenv[ $::jupyterhub::pyvenv ],
     #} ~>
     #exec { 'nbviewer-npm-install':
     #  command     => 'npm install -g',
     #  timeout     => 900,  # 15 minutes
     #  path        => $nbviewer_path,
     #  refreshonly => true,
-    #  subscribe   => Vcsrepo[$nbviewer_path],
     #} ~>
     #exec { 'invoke bower':
     #  path        => $nbviewer_path,
