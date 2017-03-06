f03be6c1dff403d89aec978aada4d8f63b464531
Bruno Marques <bruno@bmarques.net>
2017-03-06 17:43:26 -0300

    Added extra options for Debian on node.js

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,8 @@
 class jupyterhub::install {
   class { 'nodejs': # TODO: add puppet/nodejs as dependency
     repo_url_suffix => '5.x',
+    legacy_debian_symlinks => true, # TODO: restrict to Debian
+    npm_package_ensure => true,
   }
   ensure_packages(['python3-venv'])
 
