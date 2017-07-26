37309b67468b012ccab17d8d12e3c0e292f7f56c
Yorick Poels <yorick.poels@kuleuven.be>
2017-07-26 11:31:17 +0200

    Rewrite for hiera5 data in module

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,38 +6,59 @@
 # Parameters
 # ----------
 #
-# * `sample parameter`
+# @param service_name [String] Set name of the Jupyterhub service.
+# @param jupyterhub_username [String] Set username
+# @param jupyterhub_group [String] Set group
+# @param Stdlib::Absolutepath $jupyterhub_dir,
+# @param Stdlib::Absolutepath $pyvenv,
+# @param Optional[String] $allowed_users,
+# @param Integer $port,
+# @param Optional[String] $oauth_callback_url,
+# @param Optional[Boolean] $oauth_github_enable,
+# @param Optional[Integer] $oauth_github_client_id,
+# @param Optional[Integer] $oauth_github_client_secret,
+# @param Stdlid::Absolutepath $base_url,
+# @param Optional[Boolean] $sudospawner_enable,
+# @param Optional[Boolean] $systemdspawner_enable,
+# @param Optional[Stdlib::Absolutepath] $systemdspawner_user_workingdir,
+# @param Optional[Stdlib::Absolutepath] $systemdspawner_default_shell,
+# @param Optional[Integer] $systemdspawner_mem_limit,
+# @param Optional[Integer] $systemdspawner_cpu_limit,
+# @param Optional[Boolean] $systemdspawner_isolate_tmp,
+# @param Optional[Boolean] $systemdspawner_isolate_devices,
 #   Explanation of what this parameter affects and what it defaults to.
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
 class jupyterhub (
-  $service_name                    = $::jupyterhub::params::service_name,
-  $jupyterhub_username             = $::jupyterhub::params::jupyterhub_username,
-  $jupyterhub_group                = $::jupyterhub::params::jupyterhub_group,
-  $jupyterhub_dir                  = $::jupyterhub::params::jupyterhub_dir,
-  $pyvenv                          = $::jupyterhub::params::pyvenv,
-  $allowed_users                   = $::jupyterhub::params::allowed_users,
-  $port                            = $::jupyterhub::params::port,
-  $oauth_callback_url              = $::jupyterhub::params::oauth_callback_url,
-  $oauth_github_enable             = $::jupyterhub::params::oauth_github_enable,
-  $oauth_github_client_id          = $::jupyterhub::params::oauth_github_client_id,
-  $oauth_github_client_secret      = $::jupyterhub::params::oauth_github_client_secret,
-  $base_url                        = $::jupyterhub::params::base_url,
-  $sudospawner_enable              = $::jupyterhub::params::sudospawner_enable,
-  $systemdspawner_enable           = $::jupyterhub::params::systemdpawner_enable,
-  $systemdspawner_user_workingdir  = $::jupyterhub::params::systemdspawner_user_workingdir,
-  $systemdspawner_default_shell    = $::jupyterhub::params::systemdspawner_default_shell,
-  $systemdspawner_mem_limit        = $::jupyterhub::params::systemdspawner_mem_limit,
-  $systemdspawner_cpu_limit        = $::jupyterhub::params::systemdspawner_cpu_limit,
-  $systemdspawner_isolate_tmp      = $::jupyterhub::params::systemdspawner_isolate_tmp,
-  $systemdspawner_isolate_devices  = $::jupyterhub::params::systemdspawner_isolate_devices,
+  String $service_name,
+  String $jupyterhub_username,
+  String $jupyterhub_group,
+  Stdlib::Absolutepath $jupyterhub_dir,
+  Stdlib::Absolutepath $pyvenv,
+  Optional[String] $allowed_users,
+  Integer $port,
+  Optional[String] $oauth_callback_url,
+  Optional[Boolean] $oauth_github_enable,
+  Optional[Integer] $oauth_github_client_id,
+  Optional[Integer] $oauth_github_client_secret,
+  Stdlid::Absolutepath $base_url,
+  Optional[Boolean] $sudospawner_enable,
+  Optional[Boolean] $systemdspawner_enable,
+  Optional[Stdlib::Absolutepath] $systemdspawner_user_workingdir,
+  Optional[Stdlib::Absolutepath] $systemdspawner_default_shell,
+  Optional[Integer] $systemdspawner_mem_limit,
+  Optional[Integer] $systemdspawner_cpu_limit,
+  Optional[Boolean] $systemdspawner_isolate_tmp,
+  Optional[Boolean] $systemdspawner_isolate_devices,
 
 ) inherits ::jupyterhub::params {
 
   # validate parameters here
+  contain jupyterhub::install
+  contain jupyterhub::config
+  contain jupyterhub::service
 
-  class { '::jupyterhub::install': }
-  -> class { '::jupyterhub::config': }
-  ~> class { '::jupyterhub::service': }
-  -> Class['::jupyterhub']
+  Class { '::jupyterhub::install': }
+  -> Class { '::jupyterhub::config': }
+  ~> Class { '::jupyterhub::service': }
 }
