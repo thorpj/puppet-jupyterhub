0c2157c1b587b1ebef56a6278d9822e22e60e1be
Yorick Poels <yorick.poels@kuleuven.be>
2017-07-26 15:16:54 +0200

    Clean up params

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,17 +13,17 @@
 # @param Stdlib::Absolutepath $pyvenv,
 # @param Optional[String] $allowed_users,
 # @param Integer $port,
-# @param Optional[String] $oauth_callback_url,
+# @param Optional[Httpurl] $oauth_callback_url,
 # @param Optional[Boolean] $oauth_github_enable,
-# @param Optional[Integer] $oauth_github_client_id,
-# @param Optional[Integer] $oauth_github_client_secret,
-# @param Stdlid::Absolutepath $base_url,
+# @param Optional[String] $oauth_github_client_id,
+# @param Optional[String] $oauth_github_client_secret,
+# @param String $base_url,
 # @param Optional[Boolean] $sudospawner_enable,
 # @param Optional[Boolean] $systemdspawner_enable,
 # @param Optional[Stdlib::Absolutepath] $systemdspawner_user_workingdir,
 # @param Optional[Stdlib::Absolutepath] $systemdspawner_default_shell,
-# @param Optional[Integer] $systemdspawner_mem_limit,
-# @param Optional[Integer] $systemdspawner_cpu_limit,
+# @param Optional[String] $systemdspawner_mem_limit,
+# @param Optional[String] $systemdspawner_cpu_limit,
 # @param Optional[Boolean] $systemdspawner_isolate_tmp,
 # @param Optional[Boolean] $systemdspawner_isolate_devices,
 #   Explanation of what this parameter affects and what it defaults to.
@@ -53,11 +53,6 @@ class jupyterhub (
   Optional[Boolean] $systemdspawner_isolate_devices,
 
 ) {
-
-  # validate parameters here
-  contain jupyterhub::install
-  contain jupyterhub::config
-  contain jupyterhub::service
 
   Class { '::jupyterhub::install': }
   -> Class { '::jupyterhub::config': }
