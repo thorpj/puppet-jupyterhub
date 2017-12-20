5fec7436d5d606f1feffc69bd0c06f63fb5cc3f4
Yorick Poels <yorick.poels@kuleuven.be>
2017-12-20 11:22:21 +0100

    Fix source for custom oauth template

--- manifests/install.pp
+++ manifests/install.pp
@@ -98,7 +98,7 @@ class jupyterhub::install inherits jupyterhub {
 
       file { "${::jupyterhub::pyvenv}/lib/python3.4/site-packages/oauthenticator/oauth_custom.py":
         #content => epp("${module_name}/oauth_custom.py"),
-        source  => $::jupyterhub::oauth_custom_template,
+        source  => "file:///${::jupyterhub::oauth_custom_template}",
         owner   => $::jupyterhub::jupyterhub_username,
         require => Python::Pyvenv[$::jupyterhub::pyvenv],
       }
