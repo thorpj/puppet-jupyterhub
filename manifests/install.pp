0a826cf5e39d48d2581e5ec0547ecc9234e1a49a
Yorick Poels <yorick.poels@gmail.com>
2017-12-20 15:48:11 +0100

    Install requests_oauthlib - clean up dependencies - align code

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,14 +10,14 @@ class jupyterhub::install {
       nodejs_dev_package_ensure =>  'present',
       npm_package_ensure        =>  'present',
       }}
-    'Fedora' : { class {'::nodejs':
-      manage_package_repo       =>  false,
-      nodejs_dev_package_ensure =>  'present',
-      npm_package_ensure        =>  'present',
-      }}
-    'CentOS':{ class { '::nodejs':
-      repo_url_suffix => '6.x', }}
-      default: { class { '::nodejs': }}
+      'Fedora' : { class {'::nodejs':
+        manage_package_repo       =>  false,
+        nodejs_dev_package_ensure =>  'present',
+        npm_package_ensure        =>  'present',
+        }}
+        'CentOS':{ class { '::nodejs':
+          repo_url_suffix => '6.x', }}
+          default: { class { '::nodejs': }}
   }
 
   if $jupyterhub::manage_git == true {
@@ -38,76 +38,84 @@ class jupyterhub::install {
     managehome => true, 
   }
 
-  ~> file { $::jupyterhub::jupyterhub_dir:
-    ensure => directory,
-    owner  => $::jupyterhub::jupyterhub_username,
+  file { $::jupyterhub::jupyterhub_dir:
+    ensure  => directory,
+    owner   => $::jupyterhub::jupyterhub_username,
+    require => User[$::jupyterhub::jupyterhub_username], 
   }
 
-  -> python::pyvenv { $::jupyterhub::pyvenv:
+  python::pyvenv { $::jupyterhub::pyvenv:
     ensure  => present,
     version => 'system',
     owner   => $::jupyterhub::jupyterhub_username,
     group   => $::jupyterhub::jupyterhub_group,
-    #require => Package['python3-venv'],
-    }
+    require => File[$::jupyterhub::jupyterhub_dir], 
+  }
 
-    python::pip { 'jupyter':
-      pkgname    => 'jupyter',
+  python::pip { 'jupyter':
+    pkgname    => 'jupyter',
+    virtualenv => $::jupyterhub::pyvenv,
+    owner      => $::jupyterhub::jupyterhub_username,
+    require    => Python::Pyvenv[$::jupyterhub::pyvenv],
+  }
+
+  python::pip { 'jupyterhub':
+    pkgname    => 'jupyterhub',
+    virtualenv => $::jupyterhub::pyvenv,
+    owner      => $::jupyterhub::jupyterhub_username,
+    require    => Python::Pyvenv[$::jupyterhub::pyvenv],
+  }
+
+  if $::jupyterhub::sudospawner_enable {
+
+    python::pip { 'sudospawner':
+      pkgname    => 'sudospawner',
+      virtualenv => $::jupyterhub::pyvenv,
+      owner      => $::jupyterhub::jupyterhub_username,
+      require    => Python::Pyvenv[$::jupyterhub::pyvenv],
+    }
+  }
+
+  if $::jupyterhub::systemdspawner_enable {
+
+    python::pip { 'systemdspawner':
+      pkgname    => 'jupyterhub-systemdspawner',
+      virtualenv => $::jupyterhub::pyvenv,
+      owner      => $::jupyterhub::jupyterhub_username,
+      require    => Python::Pyvenv[$::jupyterhub::pyvenv],
+    }
+  }
+
+  if $::jupyterhub::oauth_enable {
+
+    python::pip { 'oauthenticator':
+      pkgname    => 'oauthenticator',
       virtualenv => $::jupyterhub::pyvenv,
       owner      => $::jupyterhub::jupyterhub_username,
       require    => Python::Pyvenv[$::jupyterhub::pyvenv],
     }
 
-    python::pip { 'jupyterhub':
-      pkgname    => 'jupyterhub',
+    python::pip { 'requests_oauthlib':
+      pkgname    => 'requests_oauthlib',
       virtualenv => $::jupyterhub::pyvenv,
       owner      => $::jupyterhub::jupyterhub_username,
       require    => Python::Pyvenv[$::jupyterhub::pyvenv],
     }
+  }
 
-    if $::jupyterhub::sudospawner_enable {
+  if $::jupyterhub::oauth_custom_enable {
 
-      python::pip { 'sudospawner':
-        pkgname    => 'sudospawner',
-        virtualenv => $::jupyterhub::pyvenv,
-        owner      => $::jupyterhub::jupyterhub_username,
-        require    => Python::Pyvenv[$::jupyterhub::pyvenv],
-      }
+    file { "${::jupyterhub::pyvenv}/lib/python3.4/site-packages/oauthenticator/oauth_custom.py":
+      #content => epp("${module_name}/oauth_custom.py"),
+      source  => "file://${::jupyterhub::oauth_custom_template}",
+      owner   => $::jupyterhub::jupyterhub_username,
+      require => Python::Pyvenv[$::jupyterhub::pyvenv],
     }
+  }
 
-    if $::jupyterhub::systemdspawner_enable {
-
-      python::pip { 'systemdspawner':
-        pkgname    => 'jupyterhub-systemdspawner',
-        virtualenv => $::jupyterhub::pyvenv,
-        owner      => $::jupyterhub::jupyterhub_username,
-        require    => Python::Pyvenv[$::jupyterhub::pyvenv],
-      }
-    }
-
-    if $::jupyterhub::oauth_enable {
-
-      python::pip { 'oauthenticator':
-        pkgname    => 'oauthenticator',
-        virtualenv => $::jupyterhub::pyvenv,
-        owner      => $::jupyterhub::jupyterhub_username,
-        require    => Python::Pyvenv[$::jupyterhub::pyvenv],
-      }
-    }
-
-    if $::jupyterhub::oauth_custom_enable {
-
-      file { "${::jupyterhub::pyvenv}/lib/python3.4/site-packages/oauthenticator/oauth_custom.py":
-        #content => epp("${module_name}/oauth_custom.py"),
-        source  => "file://${::jupyterhub::oauth_custom_template}",
-        owner   => $::jupyterhub::jupyterhub_username,
-        require => Python::Pyvenv[$::jupyterhub::pyvenv],
-      }
-    }
-
-    package { 'configurable-http-proxy':
-      ensure   => 'present',
-      provider => 'npm',
-      require  =>  Class['::nodejs'],
-    }
+  package { 'configurable-http-proxy':
+    ensure   => 'present',
+    provider => 'npm',
+    require  =>  Class['::nodejs'],
+  }
 }
