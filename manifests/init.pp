3ca33ef9d92c960425fb9836c84d4544bdcafe70
Yorick Poels <yorick.poels@kuleuven.be>
2018-06-19 15:57:20 +0200

    Add support for batchspawner module path and module python

--- manifests/init.pp
+++ manifests/init.pp
@@ -79,6 +79,8 @@ class jupyterhub (
   Optional[String] $batchspawner_host,
   Optional[String] $batchspawner_runtime,
   Optional[String] $batchspawner_memory,
+  Optional[Stdlib::Httpurl] $batchspawner_module_path,
+  Optional[String] $batchspawner_module_python,
   Optional[Boolean] $custom_packages_enable,
   Optional[Array] $custom_packages,
 
